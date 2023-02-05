b83eaa937080b3c14d858d75a1f4aa20f0d38706
Élie Goudout <eliegoudout@hotmail.com>
2023-02-05 13:27:54 +0100

    GameRules args, attr type hint, repr

--- las_vegas/core/rules.py
+++ las_vegas/core/rules.py
@@ -10,13 +10,14 @@ __all__ = ['GameRules']
 from typing import Any
 
 
-def _is_int(x: Any) -> bool:
-    return isinstance(x, int)
-
-
-def _is_int_list(xl: Any) -> bool:
+def _is_int_list(x: Any) -> bool:
     """ Returns `True` for empty list. """
-    return isinstance(xl, list) and all(map(_is_int, xl))
+    if not isinstance(x, list):
+        return False
+    for elt in x:
+        if not isinstance(elt, int):
+            return False
+    return True
 
 
 class GameRules:
@@ -46,15 +47,13 @@ class GameRules:
             num_rounds: int = 4,
             num_casinos: int = 6,  # number of faces of dice
             num_own_dice: int = 8,
-            num_xtr_dice: int | None = None,
-            num_xtr_dice_dict: dict[int, int] = {
+            num_xtr_dice: int | dict[int, int] = {
                 2: 4,
                 3: 2,
                 4: 2,
                 5: 0,
                 },
-            bills: list[int] | None = None,
-            bills_dict: dict[int, int] = {
+            bills: dict[int, int] | list[int] = {
                 10000: 5,
                 20000: 7,
                 30000: 7,
@@ -65,62 +64,62 @@ class GameRules:
                 80000: 4,
                 90000: 4,
                 },
-            casinos_min: list[int] | None = None,
-            casino_min: int = 50000) -> None:
+            casinos_min: int | list[int] = 50000) -> None:
         """ Constructor for `GameRules`.
 
         Default values are derived from rulebook.
 
         Arguments:
         ----------
-            bills (list[int] | None): List of bills to use if not
-                `None`.
-            bills_dict (dict[int, int]): Dictionnary of bills to use as
-                `bill_value: quantity` pairs, if `bills == None`.
-            casino_min (int): Target minimum value at every casino at
-                every round if `casinos_min == None`.
-            casinos_min (list[int] | None): Casino-specific target
-                minimum value at every round.
+            bills (dict[int, int] | list[int]): Bills to use in the game
+                in one of two possible formats: a dictionary of pairs
+                `<bill value>: <quantity>`, or a simple list.
+            casinos_min (int | list[int]): Target minimum amount of
+                money at each casino at every round. If of type `int`,
+                the same value will be used for every casino.
             num_casinos (int): Number of casinos (i.e. number or faces
                 of dice).
             num_own_dice (int): Number of dice owned by every player.
             num_players (int): Number of players.
             num_rounds (int): Number of rounds.
-            num_xtr_dice (int | None): Number of neutral (or 'extra')
-                dice owned by players.
-            num_xtr_dice_dict (dict[int, int]): Same as `num_xtr_dice`
-                but for multiple possible values of `num_players`. Pairs
-                `num_players: num_xtr_dice`.
+            num_xtr_dice (int | dict[int, int]): Number of neutral (or
+                'extra') dice owned by players. If provided as a `dict`,
+                the value `num_xtr_dice[num_players]` is used.
         """
         # Number of players
-        self.num_players = num_players
-        assert _is_int(self.num_players) and self.num_players > 0
+        self.num_players: int = num_players
+        assert self.num_players > 0
         # Number of rounds
-        self.num_rounds = num_rounds
-        assert _is_int(self.num_rounds) and self.num_rounds > 0
+        self.num_rounds: int = num_rounds
+        assert self.num_rounds > 0
         # Number of dice
         #   Own
-        self.num_own_dice = num_own_dice
-        assert _is_int(self.num_own_dice) and self.num_own_dice >= 0
+        self.num_own_dice: int = num_own_dice
         #   Xtr
-        self.num_xtr_dice = (
-            num_xtr_dice_dict[self.num_players]
-            if num_xtr_dice is None
+        self.num_xtr_dice: int = (
+            num_xtr_dice[self.num_players]
+            if isinstance(num_xtr_dice, dict)
             else num_xtr_dice)
-        assert _is_int(self.num_xtr_dice) and self.num_xtr_dice >= 0
-        assert self.num_own_dice + self.num_xtr_dice > 0
+        assert self.num_own_dice > 0
+        assert self.num_xtr_dice >= 0
         # Casinos
-        self.num_casinos = num_casinos
-        assert _is_int(self.num_casinos) and self.num_casinos > 0
+        self.num_casinos: int = num_casinos
+        assert self.num_casinos > 0
         # Casinos min
-        self.casinos_min = (
-            [casino_min] * self.num_casinos
-            if casinos_min is None
-            else casinos_min)
+        self.casinos_min: list[int] = (
+            casinos_min
+            if isinstance(casinos_min, list)
+            else [casinos_min] * self.num_casinos)
         assert _is_int_list(self.casinos_min)
         # Bills
-        self.bills = (
-            [bill for val, num in bills_dict.items() for bill in [val] * num]
-            if bills is None
+        self.bills: list[int] = (
+            [bill for val, qty in bills.items() for bill in [val] * qty]
+            if isinstance(bills, dict)
             else bills)
         assert _is_int_list(self.bills)
+
+    def __repr__(self):
+        cls_name = type(self).__name__
+        attr_str = ', '.join([
+            "%s=%r" % kv for kv in self.__dict__.items()])
+        return f"{cls_name}({attr_str})"
